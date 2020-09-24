4d6d91b6606ddebc954ad18cf30ef02ee2547586
nazarii_ivaniuk <nazarii.ivaniuk@axigate.fr>
2020-09-24 23:47:04 +0200

    Reordered menu Added Ability to create files Ability to delete files and directories
    Ongoing
    Share file
    
    Todo
    Upload Document
    Enter a key
    Rename document

--- src/com/stockid/Main.java
+++ src/com/stockid/Main.java
@@ -4,17 +4,18 @@ import com.stockid.os.OSValidator;
 import com.stockid.tools.FileTools;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.util.function.Consumer;
 
 public class Main {
 
-    private static Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
 
-    private static String directorySeparator;
+    public static String directorySeparator;
     private static String currentDirectory;
     private static String rootDirectory;
-
+    private static final String defPhotoExtension = ".jpg";
     private static int exitCode = 0;
 
     private static HashMap<Integer, String> choices;
@@ -45,23 +46,91 @@ public class Main {
         }
         choices.put(exitCode++, "Display Files");
         actions.put(exitCode - 1, __ -> displayFiles(rootDirectory + currentDirectory));
-        choices.put(exitCode++, "Share File");
-        choices.put(exitCode++, "Create Folder");
-        actions.put(exitCode - 1, __ -> createFolder());
-        choices.put(exitCode++, "Take a picture");
-        choices.put(exitCode++, "Upload Document");
-        choices.put(exitCode++, "Enter a key");
         choices.put(exitCode++, "Enter directory");
         actions.put(exitCode - 1, __ -> enterDirectory());
         choices.put(exitCode++, "Parent directory");
         actions.put(exitCode - 1, __ -> parentDirectory());
+        choices.put(exitCode++, "Take a picture");
+        actions.put(exitCode - 1, __ -> createFile());
+        choices.put(exitCode++, "Create Folder");
+        actions.put(exitCode - 1, __ -> createFolder());
+        choices.put(exitCode++, "Delete");
+        actions.put(exitCode - 1, __ -> deleteFile());
+        choices.put(exitCode++, "Share File");
+        actions.put(exitCode - 1, __ -> shareFile());
+        choices.put(exitCode++, "Upload Document");
+        choices.put(exitCode++, "Enter a key");
         choices.put(exitCode, "Exit");
         return true;
     }
 
+    private static void deleteFile() {
+        System.out.print("Enter file name :");
+        String fileName = scanner.next();
+        File file = new File(rootDirectory + currentDirectory + fileName);
+        if (!file.exists()) {
+            System.out.println("Error: File " + fileName + " does not exist");
+            return;
+        }
+        if (file.isDirectory()) {
+            println("File " + fileName + " is a directory,\nAre you sure you want to delete it along with "
+                    + countFiles(file) + " objects inside? (yes/n)");
+            String answer = "";
+            while (!answer.equals("yes") && !answer.equals("n")) {
+                answer = scanner.next();
+            }
+            if (answer.equals("n")) {
+                println("Aborting");
+                return;
+            }
+           deleteDir(file);
+           return;
+        }
+        if (!file.delete()) {
+            println("Error: unable to delete " + fileName);
+        }
+    }
+
+    private static void deleteDir(File file) {
+        File[] contents = file.listFiles();
+        if (contents != null)
+            for (File f : contents)
+                deleteDir(f);
+        if (!file.delete())
+         println("Error: unable delete "+FileTools.getFileName(file.getPath(),directorySeparator));
+    }
+    private static int countFiles(File file)
+    {
+        int i = 0;
+        File[] contents = file.listFiles();
+        if (contents != null)
+            for (File f : contents)
+            {
+                deleteDir(f);
+                i++;
+            }
+        return i;
+    }
+    private static void createFile() {
+        System.out.print("Pick a name for your new document: ");
+        String fileName = scanner.next() + defPhotoExtension;
+        File file = new File(rootDirectory, currentDirectory + directorySeparator + fileName);
+        if (file.exists()) {
+            System.out.println("Error: File already exist");
+            return;
+        }
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File " + fileName + " created with success");
+            }
+        } catch (IOException e) {
+            System.out.println("Error: unable to create " + fileName);
+        }
+    }
+
     private static void parentDirectory() {
         currentDirectory = FileTools.removeLastDirectory(currentDirectory, directorySeparator);
-        displayFiles(rootDirectory+currentDirectory);
+        displayFiles(rootDirectory + currentDirectory);
     }
 
     public static void main(String[] args) {
@@ -77,28 +146,33 @@ public class Main {
                            + "    \\_____\\/       \\__\\/     \\_____\\/     \\_____\\/     \\__\\/\\__\\/   \\________\\/     \\____/_/ \n"
                            + "                                                                                             ");
         int opCount = 0;
-        while (pickAction(displayMenu()))
+        while (pickAction(displayMenu())) {
             opCount++;
-        System.out.println("You have performed "+opCount+" operations.");
+        }
+        System.out.println("You have performed " + opCount + " operations.");
         scanner.close();
     }
 
     private static int displayMenu() {
         int choise = 0;
         do {
-            System.out.println("List of available actions :");
+            System.out.println("\nList of available actions :");
             for (Integer key : choices.keySet()) {
                 System.out.println(key + ") " + choices.get(key));
             }
             try {
                 choise = Integer.parseInt(scanner.next());
-            }catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error: Integer expected.\n");
             }
-
         } while (choise > exitCode);
         return choise;
+    }
+
+    private static void shareFile() {
+        displayFiles(rootDirectory + currentDirectory);
+        System.out.println("Chose a file");
+        Share.document(rootDirectory + currentDirectory + directorySeparator + scanner.next());
     }
 
     private static boolean pickAction(int userChoise) {
@@ -127,6 +201,8 @@ public class Main {
             f = "\t" + f;
             if (!f.contains(".")) {
                 f = "d" + f;
+            } else {
+                f = "f" + f;
             }
             System.out.println(f);
         }
@@ -155,5 +231,9 @@ public class Main {
             return;
         }
         currentDirectory += dirName + directorySeparator;
+    }
+
+    private static void println(String message) {
+        System.out.println(message);
     }
 }
---
+++ src/com/stockid/Share.java
@@ -0,0 +1,45 @@
+package com.stockid;
+
+import com.stockid.tools.FileTools;
+import com.sun.tools.javac.util.List;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Share {
+    private static ArrayList<String> medias = new ArrayList<>(List.of("Facebook","Instaram", "Mail","Gmail", "SMS", "Go back"));
+    public static void document(String path)
+    {
+        int choise = -1;
+        File file = new File(path);
+        String fileName = FileTools.getFileName(path, Main.directorySeparator);
+        if (!file.exists())
+        {
+            System.out.println("Error: File " + fileName +" does not exist.\n");
+            return;
+        }
+        if(file.isDirectory())
+        {
+            System.out.println("Error: File " + fileName+" is a directory.\n");
+            return;
+        }
+        System.out.println("Sharing " + fileName+"\nPick a Media:");
+        for (String s : medias) {
+            System.out.println(medias.indexOf(s)+") " + s);
+        }
+        while (choise>medias.size() || choise < 0){
+            try {
+                choise = Integer.parseInt(Main.scanner.next());
+                if(choise>medias.size() || choise < 0){
+                    System.out.println("Error: option "+choise+" does not exist\n");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: Integer Expected.\n");
+            }
+        }
+        if (choise!= medias.size()-1)
+            System.out.println("Shared "+fileName+" on "+medias.get(choise)+" with success.");
+        else
+            System.out.println("Aborting");
+    }
+}
